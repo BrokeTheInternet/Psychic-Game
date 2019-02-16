223da5a58bd13d8fb0fa40258d1bb8b5e2537b2b
iBrokeTheInternet <brandonotero14@gmail.com>
2019-02-16 04:57:18 -0500

    Psychic Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,14 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "r", "p", "s"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
 
 // Creating variables to hold the number of wins, losses, and ties. They start at 0.
+var randomLetters = [Math.floor(Math.random() * letters.length)];
+var computerChoices = letters[randomLetters];
 var wins = 0;
 var losses = 0;
 var guesses = 10;
 var used = [];
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 // Create variables that hold references to the places in the HTML where we want to display things.
 var directionsText = document.getElementById("directions-text");
@@ -16,38 +19,51 @@ var lossesText = document.getElementById("losses-text");
 var guessesText = document.getElementById("guesses-text");
 var usedText = document.getElementById("used-text");
 
-
+var reset = function () {
+  
+}
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
 
-    // Determines which key was pressed.
-    var userGuess = event.key;
+  // Determines which key was pressed.
+  var userGuess = event.key;
 
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  // Randomly chooses a choice from the options array. This is the Computer's guess.
+  var randomLetters = [Math.floor(Math.random() * letters.length)];
+  var computerGuess = letters[randomLetters];
 
-    // Reworked our code from last step to use "else if" instead of lots of if statements.
+  // Reworked our code from last step to use "else if" instead of lots of if statements.
 
-    // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-    if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s") || (userGuess === "a") || (userGuess === "b") || (userGuess === "c")) {
+  // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+  if (userGuess === "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z") {
 
-      if ((userGuess === computerGuess)) {
-        wins++;
-      } else if (userGuess !== computerGuess) {
-        guesses--;
-      } if(guesses < 0) {
-        losses++;
-      } if usedText[0].innerHTML = rightWord;
+    if (userGuess === computerGuess) {
+      wins++;
+      guesses = 9;
+      used = [];
 
-      // Hide the directions
-      directionsText.textContent = "";
+    } else if (userGuess !== computerGuess) {
+      guesses--;
+      used.push(userGuess);
 
-      // Display the user and computer guesses, and wins/losses/ties.
-      userChoiceText.textContent = "You chose: " + userGuess;
-      computerChoiceText.textContent = "I am thinking of: " + computerGuess;
-      winsText.textContent = "wins: " + wins;
-      lossesText.textContent = "losses: " + losses;
-      guessesText.textContent = "Guesses remaining: " + guesses;
-      usedText.textContent = "Your Guesses so far: " + used;
+    } if (guesses < 1) {
+      losses++;
+      
+      
+    } if (guesses === 0) {
+      guesses = 9;
+      used = [];
     }
-  };
+
+    // Hide the directions
+    directionsText.textContent = "";
+
+    // Display the user and computer guesses, and wins/losses/ties.
+    userChoiceText.textContent = "You chose: " + userGuess;
+    computerChoiceText.textContent = "I am thinking of: " + computerGuess;
+    winsText.textContent = "wins: " + wins;
+    lossesText.textContent = "losses: " + losses;
+    guessesText.textContent = "Guesses remaining: " + guesses;
+    usedText.textContent = "Your Guesses so far: " + used;
+  }
+};
